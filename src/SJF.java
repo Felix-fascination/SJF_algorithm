1396c427ff3a4144a32c075728f52196a90a9f42
Filex <meseroigo@gmail.com>
2022-12-06 02:04:16 +0300

    Initial commit

---
+++ src/SJF.java
@@ -0,0 +1,127 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.HashMap;
+import java.util.TreeMap;
+
+class SJF extends JFrame implements ActionListener {
+    JButton buttonControllers[] = new JButton[3];
+    JTextField inputTimeText[], processSizeText[];
+    TreeMap<Integer,Integer> finishTime;
+    TreeMap<Integer,Integer> waitingTime;
+    TreeMap<Integer,Integer> inputTime;
+    TreeMap<Integer,Integer> processSize;
+    JLabel processStrings[], firstMenuCols[];
+    JPanel northLayout, southLayout;
+    Container firstMenuContainer;
+    int numberOfProcesses, p;
+    String stringControllers[] = {"Расчитать", "Очистить", "Выход"};
+    String outputCols[] = {"Process ID", "Время добавления ", "Время обработки ", "Время ожидания ", "Время завершения "};
+
+
+    public SJF() {
+        super("SJF algorithm");
+        firstMenuContainer = getContentPane();
+
+        numberOfProcesses = Integer.parseInt(JOptionPane.showInputDialog("Введите количество процессов"));
+
+        firstMenuCols = new JLabel[3];
+        firstMenuCols[0] = new JLabel("Процесс");
+        firstMenuCols[1] = new JLabel("Время добавления");
+        firstMenuCols[2] = new JLabel("Время обработки");
+
+        processStrings = new JLabel[numberOfProcesses];
+        inputTimeText = new JTextField[numberOfProcesses];
+        processSizeText = new JTextField[numberOfProcesses];
+
+
+        for (int i = 0; i < numberOfProcesses; i++) {
+            processStrings[i] = new JLabel("Процесс " + (i + 1));
+            inputTimeText[i] = new JTextField(1);
+            processSizeText[i] = new JTextField(1);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            buttonControllers[i] = new JButton(stringControllers[i]);
+        }
+
+        firstMenuContainer.setLayout(new GridLayout(numberOfProcesses + 2, 3));
+        firstMenuContainer.add(firstMenuCols[0]);
+        firstMenuContainer.add(firstMenuCols[1]);
+        firstMenuContainer.add(firstMenuCols[2]);
+
+        for (int i = 0; i < numberOfProcesses; i++) {
+            firstMenuContainer.add(processStrings[i]);
+            firstMenuContainer.add(inputTimeText[i]);
+            firstMenuContainer.add(processSizeText[i]);
+        }
+        for (int i = 0; i < 3; i++) {
+            firstMenuContainer.add(buttonControllers[i]);
+            buttonControllers[i].addActionListener(this);
+        }
+    }//end of constructor
+
+    public void actionPerformed(ActionEvent ae) {
+        float sum = 0;
+        float avg;
+        JPanel main = new JPanel();
+        main.setLayout(new BorderLayout());
+        northLayout = new JPanel();
+        southLayout = new JPanel();
+        northLayout.setLayout(new GridLayout(numberOfProcesses + 1, 5));
+        southLayout.setLayout(new FlowLayout());
+
+
+        if (ae.getSource() == buttonControllers[0]) {
+            int machineTime = 1;
+            HashMap<Integer, Integer[]> processesData = new HashMap<>();
+            SJFObject processes = new SJFObject(numberOfProcesses);
+            for(int i = 0; i < numberOfProcesses; i++){
+                processes.addElement(Integer.parseInt(inputTimeText[i].getText()),
+                        Integer.parseInt(processSizeText[i].getText()));
+            }
+            for(int i = 0; i < numberOfProcesses; i++){
+                int[] results = processes.nextProcess();
+                processesData.put(results[1], new Integer[]{results[0], results[2] - 1 });
+                machineTime = machineTime + results[1];
+            }
+
+            //end for loop
+            for (int i = 0; i < 5; i++) {
+                northLayout.add(new JLabel(outputCols[i]));
+            }
+            for (int i = 0; i < numberOfProcesses; i++) {
+                northLayout.add(new JLabel("процесс " + (i + 1)));
+                northLayout.add(new JLabel("   " + Integer.parseInt(inputTimeText[i].getText())));
+                northLayout.add(new JLabel("" + Integer.parseInt(processSizeText[i].getText())));
+                northLayout.add(new JLabel("" + processesData.get(i)[1]));
+                northLayout.add(new JLabel(""+ processesData.get(i)[0]) );
+                sum += processesData.get(i)[1];
+            }
+            avg = sum / numberOfProcesses;
+            String str2 = "Среднее время ожидания " + avg;
+            southLayout.add(new JLabel(str2));
+            main.add(northLayout, BorderLayout.NORTH);
+            main.add(southLayout, BorderLayout.SOUTH);
+
+            JOptionPane.showMessageDialog(null, main, "Results", JOptionPane.PLAIN_MESSAGE);
+
+        } else if (ae.getSource() == buttonControllers[1]) {
+            setVisible(false);
+            SJF window = new SJF();
+            window.setSize(800, 300);
+            window.setVisible(true);
+            window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        } else if (ae.getSource() == buttonControllers[2]) {
+            System.exit(0);
+        }
+    }//END ACTION PERFORMED
+
+    public static void main(String[] args) {
+        SJF window = new SJF();
+        window.setSize(800, 300);
+        window.setVisible(true);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }//end main
+}//end class
---
+++ src/SJFObject.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class SJFObject {
+    private static final int MAX_MACHINE_TIME = 1000;
+    private HashMap<Integer, Map.Entry<Integer, Integer>> inputTimesProcessSize;
+    //private ArrayList<Integer> processSizes;
+    private int machineTime = MAX_MACHINE_TIME;
+    private int index = 0;
+
+    public SJFObject(int length) {
+        inputTimesProcessSize = new HashMap<>();
+        //processSizes = new ArrayList<>(length);
+    }
+
+    public void addElement(int inputTime, int processSize){
+        //inputTimes.put(index, inputTime);
+        //processSizes.add(processSize);
+        inputTimesProcessSize.put(index, Map.entry(inputTime, processSize) );
+        machineTime = Math.min(inputTime, machineTime);
+        index++;
+    }
+
+    public int[] nextProcess(){
+        ArrayList<Map.Entry<Integer, Map.Entry<Integer, Integer>>> entries = new ArrayList<>();
+        for (Map.Entry<Integer, Map.Entry<Integer, Integer>> entry: inputTimesProcessSize.entrySet()){
+            if(entry.getValue().getKey() <= machineTime) entries.add(entry);
+        }
+        int inputTime = entries.get(0).getValue().getKey();
+        int indexToDelete = entries.get(0).getKey();
+        int minSize = entries.get(0).getValue().getValue();
+        int flag = minSize;
+
+        for (Map.Entry<Integer, Map.Entry<Integer, Integer>> entry: entries){
+            minSize = Math.min(entry.getValue().getValue(), minSize);
+            if (minSize != flag) {
+                inputTime = entry.getValue().getKey();
+                indexToDelete = entry.getKey();
+            }
+            flag = minSize;
+        }
+        inputTimesProcessSize.remove(indexToDelete);
+        machineTime = machineTime + minSize;
+        return new int[]{ machineTime, indexToDelete, machineTime - minSize - inputTime + 1 };
+    }
+}
